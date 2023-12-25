b77854de14c0f8f8b7bdf70c27657191d7869763
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2023-12-25 16:10:32 +0100

    end of project

--- 06-hangman/main.py
+++ 06-hangman/main.py
@@ -1,70 +1,13 @@
 #Hangman Game! 
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\\ |
- / \\  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\\ |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\\ |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art
+import words
 
-word_list = ["aardvark", "baboon", "camel"]
 lives = 6
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = random.choice(word_list)
-print(f"the chosen word is : {chosen_word}" )
-
-
+chosen_word = random.choice(words.unique_words)
+print(hangman_art.logo)
+list_of_usen_letters = []
 display = []
 for char in chosen_word:
   display.append("_")
@@ -78,14 +21,18 @@ while not game_over:
     letter = chosen_word[position]
     if letter == guess:
       display[position] = letter
-
+  print(f"the mysterious word: {display}")
+  print(f"the used letters till now: {list_of_usen_letters}")
 
   if guess not in chosen_word:
+      if guess not in list_of_usen_letters:
+        list_of_usen_letters.append(guess)
       lives -= 1
       if lives == 0:
         game_over = True
         print("You Lose!")
-  print(stages[lives])
+        print(f"The word was: {chosen_word}")
+  print(hangman_art.stages[lives])
   if "_" not in display:
     game_over = True
     print("Yay! you WON!")
