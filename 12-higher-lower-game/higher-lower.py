b04100fd04eea1982074bbae08ca2c1cdc63ecb7
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2024-01-17 14:10:08 +0100

    importing modules, data, commenting

---
+++ 12-higher-lower-game/higher-lower.py
@@ -0,0 +1,35 @@
+####### START IMPORTS #######
+## Import Random 
+import random 
+## Import list as data from game_data
+from game_data import data
+## Import ASCII  as logo from art
+from art import logo, vs
+####### END IMPORTS #######
+
+####### START #######
+## Starting game print ASCII: Logo
+print(logo)
+## Compare A : function(name,job,residence)
+
+## Print: ASCII vs
+print(vs)
+
+## Compare B : function(name,job,residence)
+
+## input question: 'Who has more followers? Type 'a' or 'b'
+user_anser = str(input(f"Who has more followers? 'Type' 'a' or 'b': ")).lower()
+
+## ON CORRECT ANSWER
+## print ASCII: Logo
+
+## print: "You are right with user score"
+print(f"You are right! Current score : user_score")
+
+## Remove A: Insert B
+
+## ON WRONG ANSWER
+
+
+####### END #######
+
