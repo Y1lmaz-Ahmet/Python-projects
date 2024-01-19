327ba52d0b9b22a7dc7ec8ae2f368ca6936ae365
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2024-01-19 14:38:08 +0100

    adding stuff

--- 13-CoffeeMachine/main.py
+++ 13-CoffeeMachine/main.py
@@ -23,12 +23,23 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+profit = 0
 resources = {
     "coffee": 100,
     "milk": 200,
     "water": 300,
-
-
 }
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
 
